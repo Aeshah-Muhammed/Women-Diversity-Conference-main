1ae7e4d8f690906afa4f2d6cc44ed176659d8cc6
Muhammed Aisha <aishatayinke9@gmail.com>
2022-11-26 23:18:08 +0100

    commit

--- Women-Diversity-Conference-main/assets/js/script.js
+++ Women-Diversity-Conference-main/assets/js/script.js
@@ -2,16 +2,32 @@ let donateContainer = document.querySelector(".donate-container");
 let donateIcon = document.querySelector(".donate-icon");
 let donate = document.getElementById("donate");
 let container = document.querySelector(".container");
+let noneshownDonate = document.getElementById('noneshown-donate');
+
 
 
 donate.addEventListener('click',
 function()  {
   container.style.display = 'none'
-  donateContainer.style.display = 'flex'
+  donateContainer.style.display = 'block'
+  noneshownDonate.style.display = 'none'
 })
+
+
 
 donateIcon.addEventListener('click',
 function() {
 donateContainer.style.display = 'none'
+noneshownDonate.style.display = 'flex'
 container.style.display = 'block'
+
+
 })
+
+
+
+
+function click()  {
+  donateContainer.style.display = 'block'
+  noneshownDonate.style.display = 'none'
+}
